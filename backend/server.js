8142dd52ab7a2e349e343c37f31b4fbd2c5cddd3
Rabeeta Nasir <beeta.nasir@gmail.com>
2023-04-08 09:57:11 +1000

    mini edits

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,14 @@ app.use((req, res, next) => {
     next()
 })
 
-//routes
+//routeS
 app.use('/api/users', userRoutes)
 
 //connect to db
 mongoose.connect(process.env.URI)
 .then(() => {
     app.listen(process.env.PORT, () => {
-        console.log('connected to db on 4000')
+        console.log('connected to db on ', process.env.PORT)
     })
 })
 .catch((error) => {
