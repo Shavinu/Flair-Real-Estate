1e26598f01b812698e0c2b7d97cb5f9529518852
Rabeeta Nasir <beeta.nasir@gmail.com>
2023-04-08 10:00:23 +1000

    miniedits 2.0

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,42 +4,42 @@ const mongoose = require('mongoose')
 
 //get all users
 const getUsers = async (req, res) =>{
-    res.json({msg: "GET all users"})
-    // const users = await User.find().sort({CreateAt: -1})
+    // res.json({msg: "GET all users"})
+    const users = await User.find().sort({CreateAt: -1})
 
-    // res.status(200).json(users)
+    res.status(200).json(users)
 }
 
 
 //get single user
 const getaUser = async (req, res) => {
-  res.json({msg: "GET a user"})
-//     const { id } = req.params
-// if(!mongoose.Types.ObjectId.isValid(id)){
-//     return res.status(404).json({error: 'NoT a Vaild id'})
-// }
+  // res.json({msg: "GET a user"})
+    const { id } = req.params
+if(!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(404).json({error: 'NoT a Vaild id'})
+}
 
-//     const user = await User.findById(id)
+    const user = await User.findById(id)
 
-//     if(!user){
-//         return res.status(404).json({error: 'no user found'})
-//     }
+    if(!user){
+        return res.status(404).json({error: 'no user found'})
+    }
 
-//     res.status(200).json(user)
+    res.status(200).json(user)
 }
 
 
 //create new user
 const createUser = async (req, res) =>{
-  res.json({msg: "POST a user"})
-    // const {fistName, lastName, email} = req.body
+  // res.json({msg: "POST a user"})
+    const {fistName, lastName, email} = req.body
 
-    // try{
-    //     const user = await User.create({fistName, lastName, email})
-    //     res.status(200).json(user)
-    // }catch (error){
-    //     res.status(400).json({error: error.message})
-    // }
+    try{
+        const user = await User.create({fistName, lastName, email})
+        res.status(200).json(user)
+    }catch (error){
+        res.status(400).json({error: error.message})
+    }
 }
 
 //delete user
--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,27 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const userRoutes = require('./routes/users')
-const mongoose = require('mongoose')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const userRoutes = require('./routes/users');
+const mongoose = require('mongoose');
 
 //middleware
-app.use(express.json())
+app.use(express.json());
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 //routeS
-app.use('/api/users', userRoutes)
+app.use('/api/users', userRoutes);
 
 //connect to db
-mongoose.connect(process.env.URI)
-.then(() => {
+mongoose
+  .connect(process.env.URI)
+  .then(() => {
     app.listen(process.env.PORT, () => {
-        console.log('connected to db on ', process.env.PORT)
-    })
-})
-.catch((error) => {
-    console.log(error)
-})
+      console.log('connected to db on ', process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
