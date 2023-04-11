8a2d635628dc566d0d52f6afdbdc42cedb566ea1
Aruth Gardiye <20635406@student.westernsydney.edu.au>
2023-04-11 16:03:27 +1000

    added alternate license check

--- backend/routes/verify.js
+++ backend/routes/verify.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { verifyLicense } = require('../controllers/verifyIdController');
+const { altVerifyLicense } = require('../controllers/altVerifyIdController');
 
 //verify license
 router.get('/verifyLicense', verifyLicense)
+router.get('/altVerifyLicense/:id', altVerifyLicense)
 
 module.exports = router;
