b2a96ee29f766174bcbfdce9c2339d6ec2d886fd
Rabeeta Nasir <80462842+RabeetaN@users.noreply.github.com>
2023-05-22 13:42:20 +1000

    Update validation.js

--- backend/helpers/validation.js
+++ backend/helpers/validation.js
@@ -4,7 +4,7 @@ const userSchema = Joi.object({
   accType: Joi.string().required(),
   jobType: Joi.string().required(),
   licence: Joi.string().optional(),
-  verifiedLicence: Joi.boolean().optional(),
+  verifiedLicence: Joi.boolean().required(),
   email: Joi.string().email().required(),
   password: Joi.string().required(),
   firstName: Joi.string().required(),
