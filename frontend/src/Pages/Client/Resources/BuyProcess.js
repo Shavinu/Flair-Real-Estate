f4ef894074c5e8ffd116d1079e8d1f7200585aa6
harrycao911 <giahycao@gmail.com>
2023-07-09 17:21:29 +1000

    fix headline

--- frontend/src/Pages/Client/Resources/BuyProcess.js
+++ frontend/src/Pages/Client/Resources/BuyProcess.js
@@ -9,8 +9,8 @@ const BuyProcess = () => {
       <HomeHero
         cName="hero"
         heroImg="https://www.gannett-cdn.com/presto/2021/06/21/PNAS/1a51b257-cf34-4d30-a2b3-297589a51dab-Bold_Colors-10.JPG?crop=2611,1469,x1,y226&width=1600&height=800&format=pjpg&auto=webp"
-        title="Buy Process"
-        text="Choose Your New Estate with Real Estate."
+        title="Real Estate"
+        text="Buy Process"
         btnClass="hide"
         url="/"
       />
--- frontend/src/Pages/Client/Resources/MortCal.js
+++ frontend/src/Pages/Client/Resources/MortCal.js
@@ -10,8 +10,8 @@ const MortCal = () => {
       <HomeHero
         cName="hero"
         heroImg="https://www.gannett-cdn.com/presto/2021/06/21/PNAS/1a51b257-cf34-4d30-a2b3-297589a51dab-Bold_Colors-10.JPG?crop=2611,1469,x1,y226&width=1600&height=800&format=pjpg&auto=webp"
-        title="Mortgage Calculator"
-        text="Choose Your New Estate with Real Estate."
+        title="Real Estate"
+        text="Mortgage Calculator"
         btnClass="hide"
         url="/"
       />
